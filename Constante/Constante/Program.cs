6c7b0e23eb6bcb4c42c8e47f09124509b3e75c4c
oMayconLopes <maycon.0317@gmail.com>
2022-06-29 23:35:23 -0300

    Novos

---
+++ Constante/Constante/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Constante
+{
+    static void Main()
+    {
+        const string curso = "de CSharp";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Curso {0}\n{1}", curso, pi);
+    }
+}
