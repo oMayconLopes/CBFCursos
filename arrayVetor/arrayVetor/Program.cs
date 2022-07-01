85d3f33a40a11a7878c38046b17025fd96cd5f50
oMayconLopes <maycon.0317@gmail.com>
2022-06-30 23:11:57 -0300

    Attt

---
+++ arrayVetor/arrayVetor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class arrayVetor
+{
+    static void Main()
+    {
+        float[] nota = new float [4];
+        float resultado;
+
+        int i = 0;
+
+        for (i = 0; i < 4; i++) ;
+        {
+            Console.WriteLine("Informe a nota - {0}", i);
+            nota[i]=float.Parse(Console.ReadLine());
+        }
+
+        do
+        {
+            i++;
+            Console.WriteLine("A nota - {0} foi -> {1}", i, nota[i]);
+        } while (i <= 4);
+
+        
+
+    }
+}
