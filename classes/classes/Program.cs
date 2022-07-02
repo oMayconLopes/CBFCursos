8eb707c5eeedcc0a6771701f6299fb3bd88282c4
oMayconLopes <maycon.0317@gmail.com>
2022-07-01 22:13:49 -0300

    classes

---
+++ classes/classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Jogador
+{
+    public int energia = 100;
+    public bool vivo = true;
+
+}
+
+class classes
+{
+    static void Main()
+    {
+        Jogador j1 = new Jogador(); //new reserva a memória para o Objeto
+        Jogador j2 = new Jogador();
+        Jogador j3 = new Jogador();
+
+        j1.energia = 50;
+        Console.WriteLine("Energia do Jogador 1: {0}", j1.energia);
+        Console.WriteLine("Energia do Jogador 1: {0}", j2.energia);
+        Console.WriteLine("Energia do Jogador 1: {0}", j3.energia);
+    }
+}
