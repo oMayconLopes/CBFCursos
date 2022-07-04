df72b6667d9f2fd96926b97a0b1ff99ed88201a3
oMayconLopes <maycon.0317@gmail.com>
2022-07-03 23:26:55 -0300

    Novos

---
+++ construtorDestrutor/construtorDestrutor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class Jogador
+{
+    public int energia;
+    public bool vivo;
+    public string nome;
+    public Jogador(string n)
+    {
+        energia = 100;
+        vivo = true;
+        nome = n;
+    }
+    ~Jogador()
+    {
+        Console.WriteLine("Jogador {0} Destruido", nome);
+    }    
+}
+class construtorDestrutor
+{
+    static void Main()
+    {
+        string name;
+        Console.Write("Digite o nome do jogador 1: ");
+        name = Console.ReadLine();
+            
+        Jogador j1 = new Jogador(name);
+        //Jogador j2 = new Jogador("Nome 2");
+
+        j1.energia = 75;
+
+        Console.WriteLine("Energia do {1} é {0}", j1.energia, j1.nome);
+       // Console.WriteLine("Energia do {1} é {0}", j2.energia, j2.nome);
+    }
+}
---
+++ sobreCargaConstrutor/sobreCargaConstrutor/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+public class Jogador
+{
+    public int energia;
+    public bool vivo;
+    public string nome;
+   
+    public Jogador()
+    {
+        energia = 100;
+        vivo = true;
+        nome = "Jogador";
+    }
+    public Jogador(string n)
+    {
+        energia = 100;
+        vivo = true;
+        nome = n;
+    }
+    public Jogador(string n, int e)
+    {
+        energia = e;
+        vivo = true;
+        nome = n;
+    }
+    
+    public Jogador(string n, int e, bool v)
+    {
+        energia = e;
+        vivo = v;
+        nome = n;
+    }
+    public void info()
+    {
+        Console.WriteLine("Nome jogador......: {0}", nome);
+        Console.WriteLine("Energia jogador...: {0}", energia);
+        Console.WriteLine("Status jogador....: {0}\n", vivo);
+    }
+
+    ~Jogador()
+    {
+        Console.WriteLine("Jogador {0} foi destruido.", nome);
+    }
+}
+class aulaJogador
+{
+    static void Main()
+    {
+        Jogador j1 = new Jogador();
+        Jogador j2 = new Jogador("ABC");
+        Jogador j3 = new Jogador("DEF", 100);
+        Jogador j4 = new Jogador("HOG", 30, true);
+        Jogador j5 = new Jogador("ALFA", 0, false);
+
+        j1.info();
+        j2.info();
+        j3.info();
+        j4.info();
+        j5.info();
+
+    }
+}
