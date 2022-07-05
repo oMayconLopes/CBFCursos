8c7475d6c57a8023e81896bb1b7418a82e7e72b0
oMayconLopes <maycon.0317@gmail.com>
2022-07-05 15:02:49 -0300

    Novos

---
+++ metodoRetornaObjeto/metodoRetornaObjeto/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+class Galinha
+{
+    private string nomeGalinha;
+    private int numOvo;
+
+    public Galinha(string nomeGalinha)
+    {
+        this.nomeGalinha = nomeGalinha;
+        numOvo = 0;
+    }
+    public Ovo Botar() //visibilidade, tipo retorno e metodo
+    {
+        numOvo++;
+        return new Ovo(numOvo, nomeGalinha);
+    }
+}
+class galinhaOvos
+{
+    static void ovoGalinha()
+    {
+        Console.Write("Informe a quantidade de Galinhas");
+        int qtd = Convert.ToInt32(Console.ReadLine());
+        
+        for(int i = 0; i < qtd; i++)
+        {
+            
+        }
+        
+
+    }
+}
+class Ovo
+{
+    private int numOvo;
+    private string minhaGalinha;
+
+    public Ovo(int numOvo, string minhaGalinha)
+    {
+        this.numOvo = numOvo;
+        this.minhaGalinha = minhaGalinha;
+        Console.WriteLine("Ovo criado - {0} - {1}", this.numOvo, this.minhaGalinha);        
+    }
+}
+class main
+{
+    static void Main()
+    {
+        Galinha g1 = new Galinha("A");
+
+
+        g1.Botar();
+        
+    }
+
+}
---
+++ sobreCargaMetodo/sobreCargaMetodo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Calc
+{
+    public int soma(params int[]n) //sobre carga - mesmo nome com parametros diferentes.
+    {
+        int s = 0;
+        for(int i = 0; i < n.Length; i++)
+            s += n[i];
+        return s;
+    }
+    public double soma(params double[]n) //sobre carga - mesmo nome com parametros diferentes.
+    {
+        double s = 0;
+        for (int i = 0; i < n.Length; i++)
+            s+=n[i];
+        return s;
+    }
+}
+class main
+{
+    static void Main()
+    {
+        Calc calc = new Calc();
+        var somatorio = calc.soma(7.6, 9, 10);
+        Console.WriteLine(somatorio);
+    }
+}
